fc423e1453c117b0d6594b71f3f2932c13792d4e
MalcomXLi <malcom309@gmail.com>
2015-03-01 22:05:35 -0800

    Working Parse objects added

--- parse/parseHandler.js
+++ parse/parseHandler.js
@@ -14,7 +14,6 @@ var statutoryHolidays, addressCollection, collectionSchedule, streetAddress, was
 var response = require ('./response');
 
 
-
 module.exports = function(address, callback){
 		reader(function(err, data){
 			if (err){
@@ -33,65 +32,170 @@ module.exports = function(address, callback){
 		});
 }
 
+
 //function that sets the return obj
 //By the time it gets into this function all csv objects are made
 function setReturnObj (address, callback, err){
-	var addressID;
-	var scheduleCode;
-	var day;
+	var addressID, scheduleCode, day;
 	var arrayDate = [];
 	var arrayType = [];
 	var date = new Date();
-	for (i = 0 ; i < (streetAddress.street_number).length; i++){
-		if (streetAddress.street_number[i] == address.streetNum && streetAddress.street_name[i] == address.streetName)
-		{
-			//console.log (streetAddress.address_id[i]);
-			addressID = streetAddress.address_id[i];
-			if (addressID){
-				for (j = 0 ; j < (addressCollection.address_id).length; j++ ){
-					if (addressCollection.address_id[j] == addressID){
-						scheduleCode = addressCollection.schedule_code[j];
-						day = addressCollection.collection_day_array[j];
+	var i = -1, j=-1;
+	var streetAddressBool= false;
+	var scheduleCodeBool = false;
 
-						if (scheduleCode){
-							var dateSpacing = Math.abs(date.getUTCDay()+1 - parseInt(day));
+		while (!streetAddressBool){
+			i++;
+			//obtain streetaddress then flag the bool
+			if (streetAddress.street_number[i] == address.streetNum && streetAddress.street_name[i] == address.streetName){
+				addressID = streetAddress.address_id[i];
+				streetAddressBool = true;
+				
+			//bool is set, move onto next grab
+			while (!scheduleCodeBool){
+				j++;
+				//grab data for collection day and schedule code 
+				if (addressCollection.address_id[j] == addressID){
+					scheduleCode = addressCollection.schedule_code[j];
+					day = addressCollection.collection_day_array[j];
+					scheduleCodeBool = true;
+
+					//check if flag is set
+					if (scheduleCode){
+
+						var thisMonth = date.getUTCMonth()+1;
+						var thisDate = date.getUTCDate();
+						var thisDay = date.getUTCDay() + 1;
+						console.log(thisDate);
+						if (day.length > 2){//OR if addressCollection.split(';').length
+							var dateTracker = thisDay-1; //Acts as a indicator of current day
+
+							for (g = 0 ;g < (collectionSchedule.schedule_code).length; g++){
+								var dateHolder = collectionSchedule.collection_date[g].split ('/') ;
+
+								//To make sure the same day
+								if (collectionSchedule.schedule_code[g] == scheduleCode && thisMonth == parseInt(dateHolder[1]) && parseInt(dateHolder[0]) == thisDate) {
+									//If same day then we can properly proceed
+									var templocation = g;
+									console.log (templocation);
+									//starts from the location we left off at (only run like 40 times cause a month don't got more then that 31 days)
+									for (w = templocation ; w < (collectionSchedule.schedule_code).length; w++)
+									{
+
+										var tempdateHolder = collectionSchedule.collection_date[w].split ('/') ;
+											if (collectionSchedule.schedule_code[w] == scheduleCode && thisMonth== parseInt(tempdateHolder[1])){
+												//adds dateTracker
+												if (dateTracker == 7) dateTracker = 0;//reached end of the week
+												dateTracker ++;
+
+												// console.log ("Date Tracker of PROPER month : " + dateTracker);
+												// console.log ("Current date in csv  : " + collectionSchedule.collection_date[w]);
+
+												if (dateTracker != 7 && dateTracker != 1){//Not Sunday or Saturday
+													console.log (collectionSchedule.collection_date[w]);
+													arrayDate.push(collectionSchedule.collection_date[w]);
+													arrayType.push(collectionSchedule.program_code_array[w]);
+												}
+												// end of for loop you check to see if the array is good
+											}
+												if (w==(collectionSchedule.schedule_code).length-1){
+													console.log ("hey");
+													dateTracker = thisDay; //reset dateTracker
+													console.log ("dateTracker RESET VALUE : "+ dateTracker);
+												if (arrayDate.length<1){
+													for (m = 0 ; m < (collectionSchedule.schedule_code).length; m++){
+															var dateHolder = collectionSchedule.collection_date[m].split ('/') ;
+															if (collectionSchedule.schedule_code[m] == scheduleCode && thisMonth== parseInt(dateHolder[1]) && parseInt(dateHolder[0]) == thisDate) {
+																var location = m;
+																for (q = location + dateSpacing ; q < (location+40); q++)
+																{
+																	var tempdateHolder = collectionSchedule.collection_date[w].split ('/') ;
+																	if (collectionSchedule.schedule_code[q] == scheduleCode && thisMonth + 1 == parseInt(tempdateHolder[1])){
+																		//In the NEXT month
+																		console.log ("dateTracker for NONPROPER MONTH : " + dateTracker);
+																		console.log ("CURRENT CSV DATE FOR NONPROPER MONTH : " + collectionSchedule.collection_date[w]);
+																		if (dateTracker == 7) dateTracker = 0;//reached end of the week
+																		dateTracker ++;
+
+																		if (dateTracker != 7 && dateTrack != 1){
+																			arrayDate.push(collectionSchedule.collection_date[q]);
+																			arrayType.push(collectionSchedule.program_code_array[q]);
+																		}
+																	}
+																}
+															}
+														
+														if (m==(collectionSchedule.schedule_code).length-1) {
+															// console.log(arrayDate.length);
+															// console.log(arrayType.length);
+																callback(err, response(day, arrayDate, arrayType, statutoryHolidays.date));
+
+														}
+													
+													}
+												}
+												else{
+													// console.log(arrayDate.length);
+													// console.log(arrayType.length);
+													callback(err, response(day, arrayDate, arrayType, statutoryHolidays.date));
+												}
+
+											}
+
+										
+									}
+
+								}
+									
+							}
+
+
+						}
+						else {
+							var dateSpacing = Math.abs(thisDay - parseInt(day));
 							for (k = 0 ; k < (collectionSchedule.schedule_code).length; k++){
 
 								var dateHolder = collectionSchedule.collection_date[k].split ('/') ;
-								if (collectionSchedule.schedule_code[k] == scheduleCode && date.getUTCMonth()+1== parseInt(dateHolder[1]) && parseInt(dateHolder[0]) == date.getUTCDate()) {
+								if (collectionSchedule.schedule_code[k] == scheduleCode && thisMonth == parseInt(dateHolder[1]) && parseInt(dateHolder[0]) == thisDate) {
+									
 									for (w = k + dateSpacing ; w < (collectionSchedule.schedule_code).length; w+=7)
 									{
+										
 										var tempdateHolder = collectionSchedule.collection_date[w].split ('/') ;
-										if (collectionSchedule.schedule_code[w] == scheduleCode && date.getUTCMonth()+1== parseInt(tempdateHolder[1])){
-											//console.log ("hey");
+										if (collectionSchedule.schedule_code[w] == scheduleCode && thisMonth== parseInt(tempdateHolder[1])){
+											console.log ("hey");
 											arrayDate.push(collectionSchedule.collection_date[w]);
 											arrayType.push(collectionSchedule.program_code_array[w]);
 										}
 									}
 								}
 								if (k==(collectionSchedule.schedule_code).length-1) {
+
 									//checks for bad month
 									if (arrayDate.length<1){
+										
 										for (m = 0 ; m < (collectionSchedule.schedule_code).length; m++){
 											var dateHolder = collectionSchedule.collection_date[m].split ('/') ;
-											if (collectionSchedule.schedule_code[m] == scheduleCode && date.getUTCMonth()+1== parseInt(dateHolder[1]) && parseInt(dateHolder[0]) == date.getUTCDate()) {
+											if (collectionSchedule.schedule_code[m] == scheduleCode && thisMonth == parseInt(dateHolder[1]) && parseInt(dateHolder[0]) == thisDate) {
+												
 												for (w = m + dateSpacing ; w < (collectionSchedule.schedule_code).length; w+=7)
 												{
 													var tempdateHolder = collectionSchedule.collection_date[w].split ('/') ;
-													if (collectionSchedule.schedule_code[w] == scheduleCode && date.getUTCMonth()+2 == parseInt(tempdateHolder[1])){
+													if (collectionSchedule.schedule_code[w] == scheduleCode && thisMonth +1 == parseInt(tempdateHolder[1])){
 														arrayDate.push(collectionSchedule.collection_date[w]);
 														arrayType.push(collectionSchedule.program_code_array[w]);
 													}
 												}
+
 											}
 											
 											if (m==(collectionSchedule.schedule_code).length-1) {
 												// console.log(arrayDate.length);
 												// console.log(arrayType.length);
-												if (arrayDate.length < 5)
+												if (arrayDate.length < 7)
 												{
-													console.log(arrayDate);
-													(response(day, arrayDate, arrayType, statutoryHolidays.date));
+													//console.log(arrayDate);
+													callback(err, response(day, arrayDate, arrayType, statutoryHolidays.date));
 												}
 
 											}
@@ -99,8 +203,10 @@ function setReturnObj (address, callback, err){
 									}
 									else{
 										
-										if (arrayDate.length < 6)
+										if (arrayDate.length < 7)
 										{
+											console.log(arrayDate.length);
+											console.log(arrayType.length);
 											
 											callback(err, response(day, arrayDate, arrayType, statutoryHolidays.date));
 										}
@@ -109,16 +215,18 @@ function setReturnObj (address, callback, err){
 									
 								}
 							}
-
 						}
 
 					}
 
 				}
+
 			}
+			
 		}
 	}
 }
+
 
 
 
--- parse/test.js
+++ parse/test.js
@@ -2,14 +2,18 @@
 
 var parseHandler = require('./parseHandler');
 var address = {
-	streetNum : '26',
-	streetName : 'WHITE PINE WAY'
+	streetNum : '17',
+	streetName : 'WILKIE CRES'
 
 }
 var csvObjects = [];
 parseHandler(address, function(err, data){
     if(!err){
         console.log(data.garbage);
+        console.log('---------------------------------------');
+        console.log(data.dayOfWeek);
+        console.log('---------------------------------------');
+        console.log(data.holidays);
     }
     else{
         console.log(err);
