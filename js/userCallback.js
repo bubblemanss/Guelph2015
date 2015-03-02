44e9edae8131b6f502e3fe0efd3462904ad58984
Maidi Lin <maidi.lin@groupbyinc.com>
2015-03-01 23:59:32 -0500

    createUser callback works

--- js/userCallback.js
+++ js/userCallback.js
@@ -1,13 +1,29 @@
-function createUser(formData){
-	var url = "http://127.0.0.1:8080";
+function createUser(){
+	var email = document.getElementById("usermail").value;
+	var password = document.getElementById("password").value;
+	var firstName = document.getElementById("firstName").value;
+	var lastName = document.getElementById("lastName").value;
+	var streetNum = document.getElementById("streetNum").value;
+	var streetName = document.getElementById("streetName").value;
+	var postalCode = document.getElementById("postalCode").value;
+	var phoneNum = document.getElementById("phoneNum").value;
 
-	formData['address'] = {
-		streetNum : formData['streetNum'],
-		streetName : formData['streetName'],
-		postalCode : formData['postalCode']
+	var url = "http://127.0.0.1:8080";
+	console.log("form");
+
+	var formData = {};
+	formData.email = email;
+	formData.password = password;
+	formData.firstName = firstName;
+	formData.lastName = lastName;
+	formData.address = {
+		streetNum : streetNum,
+		streetName : streetName,
+		postalCode : postalCode
 	};
-	console.log(formData);
-	jQuery.post(url, formData, function(data){
+	formData.phoneNum = phoneNum;
+	console.log(JSON.stringify(formData));
+	jQuery.post(url, JSON.stringify(formData), function(data){
 		console.log(data);
 	})
 }
--- simpleServer.js
+++ simpleServer.js
@@ -5,7 +5,7 @@ var server = http.createServer(function(request, response) {
     if (request.method == "POST"){
         request.on("data", function(jsonBody){
             var parsedBody = JSON.parse(jsonBody);
-
+            console.log("in");
             response.writeHead(200, {"Content-Type": "application/json"});
             response.write(parsedBody.toString());
             response.end();
